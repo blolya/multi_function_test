3e0b851871127c88cfd37c7e02523c6eee95271a
blolya <mast-94@mail.ru>
2023-03-18 02:29:29 +0300

    change yaml source to folder

--- function1/index.mjs
+++ function1/index.mjs
@@ -2,7 +2,7 @@ export const handler = async(event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from function1!'),
+        body: JSON.stringify('Hello from function1 folder!'),
     };
     return response;
 };
