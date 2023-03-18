6d1c51073a22a9c9400c663ee2d6ddd4ab288c27
blolya <mast-94@mail.ru>
2023-03-18 20:37:24 +0300

    add multi files

---
+++ function1/index2.mjs
@@ -0,0 +1,8 @@
+export const handler = async(event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from function1 folder function 2!'),
+    };
+    return response;
+};
---
+++ function2/index2.mjs
@@ -0,0 +1,8 @@
+export const handler = async(event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from function2 folder function2!'),
+    };
+    return response;
+};
