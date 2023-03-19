6f605cfcbb426d9a00ff725b2403982e6569225a
blolya <mast-94@mail.ru>
2023-03-19 13:11:32 +0300

    test

--- function1/index2.mjs
+++ function1/index2.mjs
@@ -2,7 +2,7 @@ export const handler = async(event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from function1 folder function 2!'),
+        body: JSON.stringify('Hello from function1 folder function 2 changed!'),
     };
     return response;
 };
