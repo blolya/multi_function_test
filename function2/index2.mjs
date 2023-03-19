c1cf49b9cb50b1b383791db270f627169e352724
blolya <mast-94@mail.ru>
2023-03-19 13:21:31 +0300

    add job to check file changes

--- function2/index2.mjs
+++ function2/index2.mjs
@@ -2,7 +2,7 @@ export const handler = async(event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from function2 folder function2!'),
+        body: JSON.stringify('Hello from function2 folder function2 changed!'),
     };
     return response;
 };
